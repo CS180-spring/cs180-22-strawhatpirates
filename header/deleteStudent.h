59e44e790210d869d9dc320e1e2bebbb3f89b6fe
github-username <hovuminhmike@gmail.com>
2023-04-28 12:17:52 -0700

    new merge

--- header/deleteStudent.h
+++ header/deleteStudent.h
@@ -2,7 +2,25 @@
 
 using namespace std;
 
-void deleteStudent()
+void deleteStudent(string SID)
 {
     cout << "\nYou have chosen to delete a student" << endl;
+    string fileName;
+    ofstream fileC;
+    // cout << "Enter the name of the file you want to read: ";
+    // cin >> fileName;
+    ifstream fin("student1.json");
+    json data = json::parse(fin);
+
+    cout << "\nYou have chosen to delete a student" << endl;
+    string currentSID = data.value("SID", "not found");
+    while(SID.compare(currentSID) != 0)
+    {
+        //currentSID = next value
+    }
+
+    data.erase(SID);
+
+    return;
+
 }
