c6b43d40cc540dd5f1e96019460c9a42ac8831c5
Michael <mderi004@ucr.edu>
2023-04-28 12:23:43 -0700

    Included delete student to main

--- main.cpp
+++ main.cpp
@@ -1,98 +1,153 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+#include <limits>
+#include "json.hpp"
+#include "createfile.h"
+#include "deleteStudent.h"
 
+using namespace nlohmann;
 using namespace std;
 
-int printMenu();
-int sortMenu();
-
-//add an entry into a JSON file
-int addEntry()
+int menu()
 {
-	string name;
-	string lastName;
-	string major;
-	string year;
-	string gpa;
-	string sid;
-	string data;
-	fstream file;
-	file.open("student1.json", ios::app);
-	cout << "Enter the student's name: ";
-	cin >> name;
-	cout << "Enter the student's last name: ";
-	cin >> lastName;
-	cout << "Enter the student's major: ";
-	cin >> major;
-	cout << "Enter the student's year: ";
-	cin >> year;
-	cout << "Enter the student's GPA: ";
-	cin >> gpa;
-	cout << "Enter the student's SID: ";
-	cin >> sid;
-	file << "{\n";
-	file << "\t\"First Name\": \"" << name << "\",\n";
-	file << "\t\"Last Name\": \"" << lastName << "\",\n";
-	file << "\t\"GPA\": \"" << gpa << "\",\n";
-	file << "\t\"Major\": \"" << major << "\",\n";
-	file << "\t\"SID\": \"" << sid << "\",\n";
-	file << "\t\"Year\": \"" << year << "\"\n";
-	file << "},\n";
-	file.close();
-	return 0;
+    int menuChoice;
+
+    do{
+        cout << "\nWelcome to the UCR Student Database" << endl;
+        cout << "1. Add a student" << endl;
+        cout << "2. Delete a student" << endl;
+        cout << "3. Edit a student" << endl;
+        cout << "4. Sort students" << endl;
+        cout << "5. Search for a student" << endl;
+        cout << "6. Create a new file" << endl;
+        cout << "7. Read a file" << endl;
+        cout << "8. Exit" << endl;
+        cout << "Enter your selection: ";
+
+        cin >> menuChoice;
+        
+        if(!cin){
+            cout << "\nInvalid selection" << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+        else if (menuChoice < 1 || menuChoice > 8)
+                cout << "\nInvalid selection" << endl;
+    }while(menuChoice < 1 || menuChoice > 8);
+
+    return menuChoice;
+    
+}
+
+void addStudent()
+{
+    cout << "\nYou have chosen to add a student" << endl;
+
+    string firstName;
+    string lastName;
+    string major;
+    string year;
+    double gpa;
+    int sid;
+
+    string fileName;
+    ofstream fileC;
+    
+
+    cout << "Enter the name of the file you want to open: ";
+    cin >> fileName;
+    // fileC.open(fileName);
+
+    cout << "Enter the student's first name: ";
+    cin >> firstName;
+    cout << "Enter the student's last name: ";
+    cin >> lastName;
+    cout << "Enter the student's major: ";
+    cin >> major;
+    cout << "Enter the student's year: ";
+    cin >> year;
+    cout << "Enter the student's GPA: ";
+    cin >> gpa;
+    cout << "Enter the student's SID: ";
+    cin >> sid;
+
+    //create an empty structure (null)
+    json j = {
+    {"First name:", firstName},
+    {"Last name:", lastName},
+    {"GPA:", gpa},
+    {"Major:", major},
+    {"SID:", sid},
+    {"Year:", year}
+    };
+
+    //prints out the json object with 4 spaces
+    std::cout << j.dump(4) << std::endl;
+
+    ofstream o(fileName);
+    o << setw(4) << j << std::endl;
+
+
+}
+
+void deleteStudent()
+{
+    cout << "\nYou have chosen to delete a student" << endl;
+}
+
+void editStudent()
+{
+    cout << "\nYou have chosen to edit a student" << endl;
+}
+
+void sortStudent()
+{
+    cout << "\nYou have chosen to sort students" << endl;
+}
+
+void searchStudent()
+{
+    cout << "\nYou have chosen to search for a student" << endl;
+}
+
+
+
+void readFile()
+{
+        // string fileName;
+        // ofstream fileC;
+        // cout << "Enter the name of the file you want to read: ";
+        // cin >> fileName;
+        // ifstream i(fileName);
+        // json j;
+        // i >> j;
+        // cout << j.dump(4) << endl;
+
 }
 
 int main()
 {
-	int a = printMenu();
-	cout << "The menu selection is: " << a << endl;
-	if(a == 1)
-	{
-		addEntry();
-	}
-	return 0;
-}
+    do{
+        int a = menu();
 
-int sortMenu()
-{
-	int menuChoice;
-	cout << "6 - Sort by GPA (Ascending order): \n";
-	cout << "7 - Sort by GPA (Descending order): \n";
-	cout << "8 - Sort by Year Number (Ascending order): \n";
-	cout << "9 - Sort by Year Number (Descending order): \n";
-	cout << "10 - Sort by SID: \n";
-	cout << "11 - Sort by Major: \n";
-	cout << "12 - Sort by FIrst Name: \n";
-	cout << "13 - Sort by Last Name: \n";
-	cin >> menuChoice;
+        if(a == 1)
+            addStudent();
+        else if(a == 2)
+            deleteStudent();
+        else if(a == 3)
+            editStudent();
+        else if(a == 4)
+            sortStudent();
+        else if(a == 5)
+            searchStudent();
+        else if(a == 6)
+            createFile();
+        else if(a == 7)
+            readFile();
+        else if(a == 8)
+            exit(0);   
+    }while(true);
 
-	if(menuChoice >= 6 && menuChoice <= 13)
-	{
-		return menuChoice;
-	}
-	return sortMenu();
-}
-
-int printMenu()
-{
-	int menuChoice;
-
-	cout << "1 - Add a student: \n";
-	cout << "2 - Print student details: \n";
-	cout << "3 - Update a students info: \n";
-	cout << "4 - Remove a student: \n";
-	cout << "5 - Sort student database: \n";
-	cin >> menuChoice;
-
-	if(menuChoice >= 1 && menuChoice <= 4)
-	{
-		return menuChoice;
-	}
-	else if(menuChoice == 5)
-	{
-		return sortMenu();
-	}
-
-	return printMenu();
+    return 0;
 }
