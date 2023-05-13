3ed2a81348c21bd913142d05c9440962cd442661
Saul <mendozasaul07@gmail.com>
2023-05-13 16:44:19 -0700

    Add Student added to Json Interface

--- lib/JsonInterface.cpp
+++ lib/JsonInterface.cpp
@@ -1,2 +1,45 @@
 #include "../header/JsonInterface.h"
+#include <string>
 
+JsonInterface::JsonInterface() {
+    mode = true;
+}
+
+void JsonInterface::addStudent() {
+    string name, GPA, SID, yearNumber, major;
+    
+    cin.ignore();
+    // Get input from user for student info
+    cout << "Enter student name: ";
+    getline(cin, name);
+    cout << "Enter student GPA: ";
+    getline(cin, GPA);
+    cout << "Enter student ID: ";
+    getline(cin, SID);
+    cout << "Enter student year number: ";
+    getline(cin, yearNumber);
+    cout << "Enter student major: ";
+    getline(cin, major);
+    
+    // Create new student object with input values
+    Student newStudent(name, GPA, SID, yearNumber, major);
+    
+    // // Add new student to stuVector
+    stuVector.push_back(newStudent);
+
+
+    if(stuVector.size() == 0) {
+        cout << "No students found." << endl;
+    }
+    else {
+        for (int i = 0; i < stuVector.size(); i++) {
+            Student student = stuVector[i];
+            cout << "\nStudent #" << i + 1 << endl;
+            cout << "Name: " << student.getName() << endl;
+            cout << "GPA: " << student.getGPA() << endl;
+            cout << "SID: " << student.getSID() << endl;
+            cout << "Year Number: " << student.getYearNumber() << endl;
+            cout << "Major: " << student.getMajor() << endl;
+        }
+    }
+}
--- main.cpp
+++ main.cpp
@@ -5,25 +5,32 @@
 #include "lib/json.hpp"
 #include "header/createFile.h"
 #include "header/menu.h"
-#include "header/addStudent.h"
 #include "header/deleteStudent.h"
 #include "header/editStudent.h"
 #include "header/sortStudent.h"
 #include "header/searchStudent.h"
-#include "header/readFile.h"
+// #include "header/readFile.h"
+#include "header/JsonInterface.h"
+// #include "lib/Professor.cpp"
 #include "lib/Student.cpp"
 #include <string>
+#include <vector>
+
 
 using namespace nlohmann;
 using namespace std;
 
 int main()
 {
+    JsonInterface jsonInterface;
+
     do {
         int a = menu();
 
-        if (a == 1)
-            addStudent();
+        if (a == 1){
+            jsonInterface.addStudent();
+        }
+
         else if (a == 2)
             continue;
             //deleteStudent();
@@ -36,12 +43,12 @@ int main()
         else if (a == 6)
             createFile();
         else if (a == 7) {
-            vector<Student> data = readFileStu();
-            // vector<string> data = readFile();
-            for (int i = 0; i < data.size(); ++i) {
-                cout << data.at(i).getStudentInfo() << "\n" << endl;
-            }
-            //add info in vector to student object
+            // vector<Student> data = readFileStu();
+            // // vector<string> data = readFile();
+            // for (int i = 0; i < data.size(); ++i) {
+            //     cout << data.at(i).getStudentInfo() << "\n" << endl;
+            // }
+            // //add info in vector to student object
         }
         else if (a == 8)
             exit(0);
