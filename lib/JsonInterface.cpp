a9e827c45e1d40b57a22a7558f90295354a32b3f
Saul <mendozasaul07@gmail.com>
2023-05-16 22:15:10 -0700

    AddStudent complete

--- lib/JsonInterface.cpp
+++ lib/JsonInterface.cpp
@@ -1,11 +1,16 @@
-#include "../header/JsonInterface.h"
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <limits>
 #include <string>
+#include <vector>
+#include "../header/JsonInterface.h"
 
 JsonInterface::JsonInterface() {
     mode = true;
 }
 
-void JsonInterface::addStudent() {
+void JsonInterface::addStudent(vector<Student> &dataStu) {
     string firstName, lastName , GPA, SID, yearNumber, major;
     
     cin.ignore();
@@ -24,24 +29,48 @@ void JsonInterface::addStudent() {
     getline(cin, major);
     
     // Create new student object with input values
+    // Student s = Student(firstName, lastName, GPA, SID, yearNumber, major);
     Student newStudent(firstName, lastName, GPA, SID, yearNumber, major);
+
+    cout << newStudent.getStudentInfo() << endl;
     
     // // Add new student to stuVector
-    stuVector.push_back(newStudent);
+    dataStu.push_back(newStudent);
 
-
-    if(stuVector.size() == 0) {
-        cout << "No students found." << endl;
-    }
-    else {
-        for (int i = 0; i < stuVector.size(); i++) {
-            Student student = stuVector[i];
-            cout << "\nStudent #" << i + 1 << endl;
-            cout << "Name: " << student.getName() << endl;
-            cout << "GPA: " << student.getGPA() << endl;
-            cout << "SID: " << student.getSID() << endl;
-            cout << "Year Number: " << student.getYearNumber() << endl;
-            cout << "Major: " << student.getMajor() << endl;
+    // Write new student to file
+    ofstream file;
+    file.open("student2.json");
+    file << "{\n\t\"students\": [\n";
+    for (int i = 0; i < dataStu.size(); ++i) {
+        file << "\t\t{\n";
+        file << "\t\t\t\"First Name\": \"" << dataStu[i].getFirstName() << "\",\n";
+        file << "\t\t\t\"Last Name\": \"" << dataStu[i].getLastName() << "\",\n";
+        file << "\t\t\t\"GPA\": \"" << dataStu[i].getGPA() << "\",\n";
+        file << "\t\t\t\"SID\": \"" << dataStu[i].getSID() << "\",\n";
+        file << "\t\t\t\"Year Number\": \"" << dataStu[i].getYearNumber() << "\",\n";
+        file << "\t\t\t\"Major\": \"" << dataStu[i].getMajor() << "\"\n";
+        if(i == dataStu.size() - 1) {
+            file << "\t\t}\n";
+        }
+        else {
+            file << "\t\t},\n";
         }
     }
+    file << "\t]\n}";
+    file.close();
+
+    // if(dataStu.size() == 0) {
+    //     cout << "No students found." << endl;
+    // }
+    // else {
+    //     for (int i = 0; i < dataStu.size(); ++i) {
+    //         Student student = dataStu[i];
+    //         cout << "\nStudent #" << i + 1 << endl;
+    //         cout << "Name: " << student.getName() << endl;
+    //         cout << "GPA: " << student.getGPA() << endl;
+    //         cout << "SID: " << student.getSID() << endl;
+    //         cout << "Year Number: " << student.getYearNumber() << endl;
+    //         cout << "Major: " << student.getMajor() << endl;
+    //     }
+    // }
 }
