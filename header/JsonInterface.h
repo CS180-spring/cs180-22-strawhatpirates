a4c763d3c1bfa22e8ac1070140699eba08e4ba0a
HaseebU <46555484+HaseebU@users.noreply.github.com>
2023-06-01 14:39:46 -0700

    Update JsonInterface.h
    Organized JSON Interface H

--- header/JsonInterface.h
+++ header/JsonInterface.h
@@ -3,38 +3,62 @@
 
 #include <iostream> 
 #include <vector>
+#include <fstream>
+#include <algorithm>
+#include <limits>
+#include <string>
+#include <cstring>
+#include <bits/stdc++.h>
+
 #include "Professor.h"
 #include "Student.h"
+#include "../lib/json.hpp"
 
 using namespace std;
 
 class JsonInterface
 {
-	public:
-		JsonInterface(); //mode = true
-		vector<Student> readFileStu();
-		vector<Professor> readFileProf();
-		void printStudents();
-		void printProfessors();
-		void addProfessor();
-		void addStudent();
-		void removeProfessor();
-		void removeStudent();
-		void updateProfessor();
-		void updateStudent();
-		void editProfessor();
-		void editStudent();
-		void searchProfessor();
-		void searchStudent();
-		void sortStudent();
-		void changeMode();
-		void writeFileStu(vector<Student> theStudents);
-		void writeFileProf(vector<Professor> theProfessors);
-		bool getMode();
-	private:
-		vector<Professor> profVector;
-		vector<Student> stuVector;
-		bool mode;
+    public:
+        JsonInterface(); //mode = true
+        void writeFileStu(vector<Student> dataStu);
+        void writeFileProf(vector<Professor> dataProf);
+        vector<Student> readFileStu();
+        vector<Professor> readFileProf();
+        
+        void add();
+        void addProfessor();
+        void addStudent();
+
+        void sort();
+        void sortStudent();
+        void sortProfessor();
+
+        void search();
+        void searchProfessor();
+        void searchStudent();
+
+        void remove();
+        void removeProfessor();
+        void removeStudent();
+
+        void update();
+        void updateProfessor();
+        void updateStudent();
+
+        void edit();
+        void editProfessor();
+        void editStudent();
+
+        void print();
+        void printStudents();
+        void printProfessors();
+
+        void changeMode();
+        bool getMode();
+    private:
+        vector<Professor> dataProf;
+        vector<Student> dataStu;
+        bool mode = true;
 };
 
 #endif // JSONINTERFACE_H
